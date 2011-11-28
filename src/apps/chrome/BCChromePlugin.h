0f561e26aa8555251a18ac110f4db5ac6ab2ac5a
Brian Collins <bricollins@gmail.com>
2011-11-27 17:14:30 -0800

    Remove comment block

--- src/apps/chrome/BCChromePlugin.h
+++ src/apps/chrome/BCChromePlugin.h
@@ -1,11 +1,3 @@
-//
-//  BCChromePlugin.h
-//  Focus
-//
-//  Created by Brian Collins on 11-11-26.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import "BCAppPlugin.h"
 
 @interface BCChromePlugin : BCAppPlugin
